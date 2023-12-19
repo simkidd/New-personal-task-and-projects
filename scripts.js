dfb5ce3ecf1150b9c6b413493bfab5a5e7a975bb
kponky <ikponkedet@gmail.com>
2023-12-19 22:33:04 +0100

    js added

--- scripts.js
+++ scripts.js
@@ -1,20 +1,20 @@
-// const menuBtn = document.getElementById("menu-btn");
-// const navLinks = document.getElementById("nav-links");
-// const menuBtnIcon = menuBtn.querySelector("i");
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
 
-// menuBtn.addEventListener("click", (e) =>{
-//   navLinks.classList.toggle("open");
+menuBtn.addEventListener("click", (e) =>{
+  navLinks.classList.toggle("open");
 
-//   const isOpen = navLinks.classList.contains("open");
-//   menuBtnIcon.setAttribute(
-//     "class",
-//      isOpen ? "fa fa-close" : "fa fa-bars" //toggle nav-meu
-//   )
-// });
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute(
+    "class",
+     isOpen ? "fa fa-close" : "fa fa-bars" //toggle nav-meu
+  )
+});
 
-// navLinks.addEventListener("click", (e) =>{
-//   navLinks.classList.remove("open");
-//   menuBtnIcon.setAttribute("class", "fa fa-bars")
-// })
+navLinks.addEventListener("click", (e) =>{
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "fa fa-bars")
+});
 
 
